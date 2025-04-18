d11628f1ab97f3f95e456e209dd5927bdc3d28c3
bohdan.snopok <snopokbogdan3@gmail.com>
2025-04-18 23:29:15 +0300

    add nav links in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,46 +11,61 @@ export default function Header() {
                 ДОСТУПНА СРОЧНАЯ ДОСТАВКА ПО КИЕВУ - <a href="#">ДЕТАЛЬНЫЕ УСЛОВИЯ</a>
             </div>
 
-            <div className="container headerContent">
-                <div className="logo">
-                    <Image src={logo} alt="logo" />
-                    <p>винная мельница</p>
-                </div>
+            <div className="container">
+                <div className="headerContent">
+                    <div className="logo">
+                        <Image src={logo} alt="logo" />
+                        <p>винная мельница</p>
+                    </div>
 
-                <form>
-                    <input type="text" className="headerInput" />
-                    <button type="submit" className="headerInput__submit">
-                        <Image src={search} alt="search icon" />
-                    </button>
-                </form>
+                    <form>
+                        <input type="text" className="headerInput" />
+                        <button type="submit" className="headerInput__submit">
+                            <Image src={search} alt="search icon" />
+                        </button>
+                    </form>
 
-                <div className="headerContent__items">
-                    <div className="headerContent__items__item">
-                        <div className="headerContent__items__item__links">
-                            <a href="">Регистрация</a>
-                            <a href="">Вход</a>
+                    <div className="headerContent__items">
+                        <div className="headerContent__items__item">
+                            <div className="headerContent__items__item__links">
+                                <a href="">Регистрация</a>
+                                <a href="">Вход</a>
+                            </div>
+                            <a href="#">Личный кабинет</a>
                         </div>
-                        <a href="#">Личный кабинет</a>
+                    </div>
+
+                    <div className="headerContent__items">
+                        <div className="headerContent__items__item">
+                            <div className="headerContent__items__item__top">
+                                <Image src={geo} alt="geoIcon" />
+                                <div className="geo">Москва</div>
+                            </div>
+                            <a href="#">063 658 32 21</a>
+                        </div>
+                    </div>
+
+                    <div className="headerContent__items">
+                        <div className="headerContent__items__item">
+                            <div className="headerContent__items__item__top">
+                                <a href="#" className="geo">Корзина: 0</a>
+                            </div>
+                            <div className="cartPrice">0 р</div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="headerContent__items">
-                    <div className="headerContent__items__item">
-                        <div className="headerContent__items__item__top">
-                            <Image src={geo} alt="geoIcon" />
-                            <div className="geo">Москва</div>
-                        </div>
-                        <a href="#">063 658 32 21</a>
-                    </div>
-                </div>
-
-                <div className="headerContent__items">
-                    <div className="headerContent__items__item">
-                        <div className="headerContent__items__item__top">
-                            <a href="#" className="geo">Корзина: 0</a>
-                        </div>
-                        <div className="cartPrice">0 р</div>
-                    </div>
+                <div className="header__navs">
+                    <a href="#">Вино</a>
+                    <a href="#">Игристое</a>
+                    <a href="#">Виски</a>
+                    <a href="#">Коньяк</a>
+                    <a href="#">Арманьяк</a>
+                    <a href="#">Ром</a>
+                    <a href="#">Водка</a>
+                    <a href="#">Ликер</a>
+                    <a href="#">Коктейли</a>
+                    <a href="#">Деликатесы</a>
                 </div>
             </div>
         </header>
