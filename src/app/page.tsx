55ef1b1d69a3c90d82e36b1d12c170ce4608933d
bohdan.snopok <snopokbogdan3@gmail.com>
2025-04-26 17:16:41 +0300

    add subscribe component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BannerSlider from "@/components/BannerSlider/SpecialOfferSlider";
 import Offers from "@/components/Offers/Offers";
+import Subscribe from "@/components/Subscribe/Subscribe";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <BannerSlider />
       <Offers />
+      <Subscribe />
     </>
   );
 }
