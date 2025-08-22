7db4dae74e9f2056d4f41047d498ea8832e33b09
bohdan.snopok <snopokbogdan3@gmail.com>
2025-08-22 16:45:40 +0300

    add pages to footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,41 +1,44 @@
-import Link from 'next/link';
-import './Footer.scss'
+import Link from "next/link";
+import "./Footer.scss";
 
 export default function Footer() {
-    return (
-        <footer className='container footer'>
-            <div className="footer__flexWrapper">
-                <div className='footer__column'>
-                    <div className="footer__column__title">Shop</div>
-                    <a href="#">History</a>
-                    <a href="#">Michel</a>
-                    <a href="#">Values</a>
-                    <a href="#">Biodynamics</a>
-                    <p className='copyright'>2021 winemill</p>
-                </div>
+  const year = new Date().getFullYear();
 
+  return (
+    <footer className="container footer">
+      <div className="footer__flexWrapper">
+        <div className="footer__column">
+          <div className="footer__column__title">Shop</div>
+          <Link href="/catalog">History</Link>
+          <Link href="/catalog">Michel</Link>
+          <Link href="/catalog">Values</Link>
+          <Link href="/catalog">Biodynamics</Link>
+          <p className="copyright">© {year} WineMill. Все права защищены</p>
+        </div>
 
-                <div className="footer__columns">
-                    <div className='footer__column'>
-                        <div className="footer__column__title">Shop</div>
-                        <a href="#">All Wines</a>
-                        <a href="#">Single vineyard selection</a>
-                        <Link href="/privacyPolice" className='copyright copyright_link'>Privacy police</Link>
-                    </div>
+        <div className="footer__columns">
+          <div className="footer__column">
+            <div className="footer__column__title">Shop</div>
+            <Link href="/catalog">All Wines</Link>
+            <a href="#">Single vineyard selection</a>
+            <Link href="/privacyPolice" className="copyright copyright_link">
+              Privacy police
+            </Link>
+          </div>
 
-                    <div className='footer__column'>
-                        <div className="footer__column__title">The Wines</div>
-                        <a href="#">Hermitage in 3D</a>
-                        <a href="#">The soul of M. CHAPOUTIER</a>
-                    </div>
+          <div className="footer__column">
+            <div className="footer__column__title">The Wines</div>
+            <a href="#">Hermitage in 3D</a>
+            <a href="#">The soul of M. CHAPOUTIER</a>
+          </div>
 
-                    <div className='footer__column'>
-                        <div className="footer__column__title">Wine Tourism</div>
-                        <a href="#">Wine tasting services and visits</a>
-                        <a href="#">Bike ride</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+          <div className="footer__column">
+            <div className="footer__column__title">Wine Tourism</div>
+            <Link href="/wineTasting">Wine tasting services and visits</Link>
+            <Link href="/bikeRide">Bike ride</Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
