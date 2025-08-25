b6d766a3387dce5e40c83dbb876d537336c742e7
bohdan.snopok <snopokbogdan3@gmail.com>
2025-08-25 12:20:42 +0300

    add coctails details

--- src/components/CocktailsCatalog/CocktailsCatalog.tsx
+++ src/components/CocktailsCatalog/CocktailsCatalog.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import { Drink } from '@/types/Drinks';
 import { useCocktails } from '@/hooks/useCocktails';
 import { useCartStore } from '@/store/cartStore';
+import Link from 'next/link';
 
 const CocktailsCatalog: React.FC = () => {
   const { cocktailsDrinks, isLoading, error } = useCocktails();
@@ -25,7 +26,7 @@ const CocktailsCatalog: React.FC = () => {
 
           {!isLoading && !error && cocktailsDrinks.length > 0 ? (
             cocktailsDrinks.map((drink: Drink) => (
-              <div key={drink.id} className="catalog__list__card">
+              <Link href={`/product/${drink.id}`} key={drink.id} className="catalog__list__card">
                 <div className="catalog__list__card__product__blackDecor"></div>
                 <div className="catalog__list__card__product">
                   <Image src={drink.image} alt='' width={133} height={320} className='catalog__list__card__image' />
@@ -53,7 +54,7 @@ const CocktailsCatalog: React.FC = () => {
                     liters: drink.litres || 0,
                   })}>В КОРЗИНУ</button>
                 </div>
-              </div>
+              </Link>
             ))
           ) : (
             !isLoading && !error && (
